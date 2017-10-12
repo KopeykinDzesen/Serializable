9bafca614dd9db9ecf9c2b9b128ac4094fbc2987
KopeykinDzesen <kopeykin.dzesen@mail.ru>
2017-10-12 19:46:18 +0300

    massage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,25 @@
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        User user = new User();
+        user.hp = 22;
+        User newUser = new User();
+        newUser.hp = 0;
 
-       A a = new A();
-       B b = new B();
-       C c = new C();
-       D d = new D();
+        FileOutputStream fileOutputStream = new FileOutputStream("tempFile");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(user);
+        objectOutputStream.close();
 
-       printCheckType(a);
-       printCheckType(b);
-       printCheckType(c);
-       printCheckType(d);
+        FileInputStream fileInputStream = new FileInputStream("tempFile");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        newUser = (User) objectInputStream.readObject();
+        objectInputStream.close();
 
-        System.out.println("  ");
+        System.out.println(newUser.hp);
 
-    }
-
-    static <T> boolean checkTypeA(T sometype){
-        return sometype instanceof A;
-    }
-
-    static <T> boolean checkTypeB(T sometype){
-        return sometype instanceof B;
-    }
-
-    static <T> boolean checkTypeC(T sometype){
-        return sometype instanceof C;
-    }
-
-    static <T> boolean checkTypeD(T sometype){
-        return sometype instanceof D;
-    }
-
-    static <T> void printCheckType(T sometype){
-        System.out.println(sometype.getClass() + " instanceof A, B, C, D: " +
-                checkTypeA(sometype) + " " +
-                checkTypeB(sometype) + " " +
-                checkTypeC(sometype) + " " +
-                checkTypeD(sometype));
     }
 
 }
-
-class A{}
-class B extends A{}
-class C extends A{}
-class D extends B{}
