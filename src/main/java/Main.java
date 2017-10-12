a17655d3db79bdc44b992cb712e895c2c14cb4ac
KopeykinDzesen <kopeykin.dzesen@mail.ru>
2017-10-12 20:01:36 +0300

    massage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,20 +5,25 @@ public class Main {
     public static void main(String[] args) throws Exception{
         User user = new User();
         user.hp = 22;
-        User newUser = new User();
-        newUser.hp = 0;
+        User.staticField = 100;
+        Sword sword = new Sword();
+        sword.level = 10;
+        user.sword = sword;
 
         FileOutputStream fileOutputStream = new FileOutputStream("tempFile");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
         objectOutputStream.writeObject(user);
         objectOutputStream.close();
 
+        User.staticField = 200;
+
         FileInputStream fileInputStream = new FileInputStream("tempFile");
         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-        newUser = (User) objectInputStream.readObject();
+        User newUser = (User) objectInputStream.readObject();
         objectInputStream.close();
 
-        System.out.println(newUser.hp);
+        System.out.println(newUser.hp + " " + newUser.sword.level);
+        System.out.println(newUser.staticField);
 
     }
 
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,4 +2,6 @@ import java.io.Serializable;
 
 public class User implements Serializable{
     int hp;
+    Sword sword;
+    static int staticField;
 }
